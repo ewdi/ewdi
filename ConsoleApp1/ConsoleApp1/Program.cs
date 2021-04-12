f1239dee9f69ec62167ce080895a4322448b0b80
ewdi <82412261+ewdi@users.noreply.github.com>
2021-04-12 22:41:51 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                //1.soru
+                int[] dizi = new int[50];
+
+                //    Console.Write("Sayı Girin : ");
+                //    int sayi = Convert.ToInt32(Console.ReadLine());
+                //    int sonuc =  RnArray(sayi);
+                //    if (sonuc == 0)
+                //    {
+                //        Console.WriteLine("{0} Asal Sayı Değildir.", sayi);
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("{0} Asal Sayıdır.", sayi);
+                //    }
+                //    Console.ReadKey();
+                //}
+
+                //int RnArray(int sayi)
+                //{
+                //    int i;
+                //    for (i = 2; i <= sayi - 1; i++)
+                //    {
+                //        if (sayi % i == 0)
+                //        {
+                //            return 0;
+                //        }
+                //    }
+                //    if (i == sayi)
+                //    {
+                //        return 1;
+                //    }
+                //    return 0;
+
+                //2.soru
+
+                //int a, sonuc = 1;
+
+                //Console.WriteLine(":");
+                //a = Convert.ToInt32(Console.ReadLine());
+
+                //for (int i = 1; i <= a; i++)
+                //{
+                //    sonuc = i * sonuc;
+                //}
+
+                //Console.WriteLine("Sonuç: " + sonuc);
+
+                //Console.ReadKey();
+            }
+
+
+        }
+    }
+}
